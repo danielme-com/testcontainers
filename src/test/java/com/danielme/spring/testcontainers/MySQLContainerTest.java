9d185baadb5beb1e53a187221d66d9324893ff46
dani <danielme_com@yahoo.com>
2023-06-10 20:22:34 +0200

    @ServiceConnection

--- src/test/java/com/danielme/spring/testcontainers/MySQLContainerTest.java
+++ src/test/java/com/danielme/spring/testcontainers/MySQLContainerTest.java
@@ -3,6 +3,7 @@ package com.danielme.spring.testcontainers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -22,18 +23,21 @@ class MySQLContainerTest {
     private DataSource dataSource;
 
     @Container
+    @ServiceConnection
     private static final MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.0.30")
             .withDatabaseName("testcontainer")
             .withUsername("user")
             .withPassword("pass")
             .withInitScript("init.sql");
 
-    @DynamicPropertySource
+    //As of Spring Boot 3.1, with @ServiceConnection
+    // you don't need the following method to set up the connection
+    /*@DynamicPropertySource
     private static void setupProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mySQLContainer::getUsername);
         registry.add("spring.datasource.password", mySQLContainer::getPassword);
-    }
+    }*/
 
     @Test
     void testMySQLContainerIsRunning() {
--- src/test/java/com/danielme/spring/testcontainers/reuse/MySQLContainerBaseTest.java
+++ src/test/java/com/danielme/spring/testcontainers/reuse/MySQLContainerBaseTest.java
@@ -2,6 +2,7 @@ package com.danielme.spring.testcontainers.reuse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -15,6 +16,7 @@ public class MySQLContainerBaseTest {
     @Autowired
     protected DataSource dataSource;
 
+    @ServiceConnection
     protected static final MySQLContainer mySQLContainer = new MySQLContainer<>("mysql:8.0.30")
             .withDatabaseName("testcontainer")
             .withUsername("user")
@@ -27,11 +29,13 @@ public class MySQLContainerBaseTest {
         mySQLContainer.start();
     }
 
+    //As of Spring Boot 3.1, with @ServiceConnection you don't need the following method to set up the connection
+   /*
     @DynamicPropertySource
     private static void setupProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mySQLContainer::getUsername);
         registry.add("spring.datasource.password", mySQLContainer::getPassword);
     }
-
+    */
 }
